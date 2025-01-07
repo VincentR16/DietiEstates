e06d40e6a87066e3c7964a9e20913cde8084f01a
VincentR16 <vincy22.vr@gmail.com>
2025-01-07 12:19:20 +0100

    Initial commit

---
+++ src/test/java/INGSW/example/demo/ProgettoIngswApplicationTests.java
@@ -0,0 +1,13 @@
+package INGSW.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoIngswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
