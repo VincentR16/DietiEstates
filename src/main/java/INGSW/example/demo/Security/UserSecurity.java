a39c60d3d80a577c5abcd8ab8972ca2ae4a59a80
VincentR16 <vincy22.vr@gmail.com>
2025-01-08 10:57:59 +0100

    Una prova dell autenticazione

--- src/main/java/INGSW/example/demo/Security/UserSecurity.java
+++
@@ -1,6 +0,0 @@
-package INGSW.example.demo.Security;
-
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-
-public class UserSecurity extends UserDetailsServiceAutoConfiguration {
-}
